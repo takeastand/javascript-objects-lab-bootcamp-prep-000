520708073c471bc94335af979228a774dced95cb
christopher.tero Tero <christopher.tero@gmail.com>
2019-01-11 04:48:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var recipes = new Object({});
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  Object.assign(object, {[key]: value});
   object = new Object({key: value});
-  return object;
+  return Object.assign(object, {[key]: value});
 }
