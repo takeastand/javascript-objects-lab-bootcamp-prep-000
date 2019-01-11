587b6531ac0873f7be977c9f10231605cd1ee937
christopher.tero Tero <christopher.tero@gmail.com>
2019-01-11 05:16:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function deleteFromObjectByKey(object, key) {
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object.key;
+  object = delete object.key;
   return new Object([]);
 }
